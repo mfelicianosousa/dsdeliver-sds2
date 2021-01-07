e28cabea2f3a5ecd1762e0e43907f0ccf97ff596
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-01-07 19:00:37 -0400

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
+import Home from './Home';
 
 
 function App() {
   return (
     <div>
      <NavBar />
+     <Home />
      
     </div>
     
