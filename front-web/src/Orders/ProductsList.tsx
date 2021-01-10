7c11561bf406c30f71c0a2c44f5b1513782e9bc1
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-01-10 10:24:00 -0400

    Finish front-web

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 import { Product } from './types';
+import { checkIsSelected } from './Helpers';
 
 type Props = {
     products : Product[] ;
+    selectedProducts: Product[] ;
+    onSelectProduct: ( product : Product ) => void ;
 }
 
-function ProductsList({ products }:Props){
+function ProductsList({ products, selectedProducts, onSelectProduct }:Props){
     return(
-        <div className="orders-list-container">
-            <div className="orders-list-content">
-                {products.map( product=>(
-                    <ProductCard key={product.id} product={product} />
-                ))}
+        <div className = "orders-list-container">
+            <div className = "orders-list-content">
+                { products.map( product => (
+                    <ProductCard 
+                       key = { product.id} 
+                       product = { product} 
+                       onSelectProduct = { onSelectProduct }
+                       isSelected={checkIsSelected( selectedProducts, product )}
+                       
+                       />
+                ) ) }
             </div>
         </div>
 
--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,3 +1,4 @@
+import { OrderPayLoad } from './types';
 import axios from "axios" ;
 
 const API_URL = 'http://localhost:8081' ;
@@ -12,4 +13,8 @@ export function fetchProducts(){
 export function fetchLocalMapBox( local : string ){
     return axios( `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${ mapboxToken }` ) ;
 
+}
+
+export function saveOrder( payload: OrderPayLoad ){
+    return axios.post(`${API_URL}/orders`, payload) ;
 }
